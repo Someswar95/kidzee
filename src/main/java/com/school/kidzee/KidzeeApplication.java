24007d4da0d2f5e0a0c4a5a7674b3b329dca089c
Someswar Roy <someswarroy95@gmail.com>
2022-08-10 10:17:45 +0530

    make it better

--- src/main/java/com/school/kidzee/KidzeeApplication.java
+++ src/main/java/com/school/kidzee/KidzeeApplication.java
@@ -1,13 +1,21 @@
 package com.school.kidzee;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
-public class KidzeeApplication {
+import com.school.kidzee.entity.Admin;
+import com.school.kidzee.repository.AdminRepo;
 
+@SpringBootApplication
+public class KidzeeApplication implements CommandLineRunner{
+
+	@Autowired
+	private AdminRepo adminRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(KidzeeApplication.class, args);
 	}
@@ -15,6 +23,19 @@ public class KidzeeApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Admin admin = new Admin();
+		admin.setAdminId(1);
+		admin.setUsername("admin");
+		admin.setPassword("admin");
+		
+		this.adminRepo.save(admin);
+		
+		
 	}
 
 }
